f9407c0af5603ee4c65383199f1b8e2b7f3f1cc3
Nitesh Kumar <niteshk@geekyants.com>
2022-10-15 22:24:28 +0530

    Testing android studio Version control system

--- app/src/main/java/com/example/androiddemo1/KotlinBasics/KotlinProgrammerDictionary.kt
+++ app/src/main/java/com/example/androiddemo1/KotlinBasics/KotlinProgrammerDictionary.kt
@@ -1,3 +1,4 @@
-package com.example.androiddemo1.KotlinBasics/// https://blog.kotlin-academy.com/kotlin-programmer-dictionary-function-vs-method-vs-procedure-c0216642ee87
+
+/// https://blog.kotlin-academy.com/kotlin-programmer-dictionary-function-vs-method-vs-procedure-c0216642ee87
 
 /// This will have common terminologies used in Kotlin
